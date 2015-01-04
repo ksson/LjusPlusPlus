9f99b13b9ffd73cda20a55152a581551a7053b6f
ksson <micke_ksson@hotmail.com>
2015-01-04 16:29:18 +0100

    config files added for easy variable changes

--- go/pinoccioFowarding/curlFilter.go
+++ go/pinoccioFowarding/curlFilter.go
@@ -1,50 +1,46 @@
 package main
 
-import(
-	"os/exec"
+import (
 	"fmt"
+	"os/exec"
 	"strings"
 )
 
-type jsonData struct{
+type jsonData struct {
 	cmdType string
 	cmdData []string
 }
 
-func (jd jsonData) filter(troop string, leaderId string) {
+func (jd jsonData) filter(troop, leaderId, wToken string) {
 	var forwardCmd string
 	var iF insertInfo
 	iF.Data[2] = troop
 	db := openDB()
-	writeToken := "6c5da8cf710ba4d745b8b00b523a9f6a"
+	writeToken := wToken
 	fmt.Println("Preparing to forward command...")
-	if(strings.Contains(jd.cmdType, "sit")) {
-		forwardCmd = jd.cmdType+"("+jd.cmdData[0]+","+jd.cmdData[1]+")"
-		fmt.Println("Troop:"+ troop + "\nCommand: "+forwardCmd+"("+jd.cmdData[0]+")\n")
+	if strings.Contains(jd.cmdType, "sit") {
+		forwardCmd = jd.cmdType + "(" + jd.cmdData[0] + "," + jd.cmdData[1] + ")"
+		fmt.Println("Troop:" + troop + "\nCommand: " + forwardCmd + "(" + jd.cmdData[0] + ")\n")
 		seat := jd.cmdData[0]
 		iF.Table, iF.Data[0], iF.Data[1] = "benchData", "1", seat
 		iF.insertData(db)
-	}else if(strings.Contains(jd.cmdType,"uns")){
-		forwardCmd = jd.cmdType+"("+jd.cmdData[0]+")"
-		fmt.Println("Troop:"+ troop + "\nCommand: "+forwardCmd+"("+jd.cmdData[0]+")\n")
+	} else if strings.Contains(jd.cmdType, "uns") {
+		forwardCmd = jd.cmdType + "(" + jd.cmdData[0] + ")"
+		fmt.Println("Troop:" + troop + "\nCommand: " + forwardCmd + "(" + jd.cmdData[0] + ")\n")
 		seat := jd.cmdData[0]
 		iF.Table, iF.Data[0], iF.Data[1] = "benchData", "0", seat
 		iF.insertData(db)
 	}
-	c := "https://api.pinocc.io/v1/" + troop + "/"+leaderId+"/command/" + forwardCmd + "?token=" + writeToken
+	c := "https://api.pinocc.io/v1/" + troop + "/" + leaderId + "/command/" + forwardCmd + "?token=" + writeToken
 	fmt.Println(c)
 	runCmd(c)
 }
 
-func runCmd(cmd string){
-    	out, err := exec.Command("curl",cmd ).Output()
-    	if err != nil {
-        	fmt.Println("error occured")
-        	fmt.Printf("%s", err)
-    	}
+func runCmd(cmd string) {
+	out, err := exec.Command("curl", cmd).Output()
+	if err != nil {
+		fmt.Println("error occured")
+		fmt.Printf("%s", err)
+	}
 	fmt.Println(string(out))
 }
-
-
-
-
--- go/pinoccioFowarding/openDB.go
+++ go/pinoccioFowarding/openDB.go
@@ -1,18 +1,42 @@
 package main
 
-import(
-	
-      	"github.com/ziutek/mymysql/mysql"
-    	_ "github.com/ziutek/mymysql/native" // Native engine
-    	// _ "github.com/ziutek/mymysql/thrsafe" // Thread safe engine
+import (
+	"github.com/ziutek/mymysql/mysql"
+	_ "github.com/ziutek/mymysql/native" // Native engine
+	// _ "github.com/ziutek/mymysql/thrsafe" // Thread safe engine
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
+type DB_config struct {
+	DB_init []string
+}
+
+var adress, db_user, db_pwd, db_name string
+var configSet bool
 
 func openDB() mysql.Conn {
-	db := mysql.New("tcp", "", "127.0.0.1:3306","root", "jaam","D7017E")
+
+	if configSet != true {
+		initConfig()
+	}
+	db := mysql.New("tcp", "", adress, db_user, db_pwd, db_name)
 	err := db.Connect()
 	if err != nil {
 		panic(err.Error())
 	}
 	return db
+}
+
+func initConfig() {
+	file, _ := os.Open("db_config.json")
+	decoder := json.NewDecoder(file)
+	config := DB_config{}
+	err := decoder.Decode(&config)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	adress, db_name, db_user, db_pwd = config.DB_init[0], config.DB_init[1], config.DB_init[2], config.DB_init[3]
+	configSet = true
 }
